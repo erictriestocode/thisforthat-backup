f8211362df39a9e015d261daf935b2c517737e0e
Eric <eric.northrop@gmail.com>
2019-05-10 09:09:33 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// MAIN SERVER FILE
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res) => res.send('API Running!'));
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
