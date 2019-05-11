df92575385fe193efb3cfb6858c179608664fc35
Eric <eric.northrop@gmail.com>
2019-05-10 22:21:44 -0500

    App rebuild with updated components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
+// Import Components
+import Navbar from "./components/NavBar";
+import Home from './components/Home';
+import SigninForm from './components/SigninForm';
+import LandingPage from "./components/landingPage";
+import Help from './components/Help';
+import Registration from './components/Registration';
+import Transaction from './components/Transaction';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/signinform" component={SigninForm} />
+            <Route path="/home" component={Home} />
+            <Route path="/help" component={Help} />
+            <Route path="/registration" component={Registration} />
+            <Route path="/transaction" component={Transaction} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
