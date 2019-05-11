370354b62ee710e0f8e35005175d780ee32d9e9c
Eric <eric.northrop@gmail.com>
2019-05-10 21:50:10 -0500

    File Delete

--- routes/api/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// GET for api/profile
-// profile route
-// Public access
-router.get('/',(req,res) => res.send('profile Route'));
-
-module.exports = router;
--- routes/api/transactions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// GET for api/transaction
-// transaction route
-// Public access
-router.get('/',(req,res) => res.send('transaction route Route'));
-
-module.exports = router;
