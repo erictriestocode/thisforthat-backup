ee14db24945151e5e363d2696469ce0396ac28f1
Shelby Marks <44624997+ShelbyMarks@users.noreply.github.com>
2019-05-11 21:49:49 -0500

    Update index.js

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -4,86 +4,58 @@ import "./style-profile.css";
 
 class Profile extends Component {
     render() {
-    return (
-        <div class="wrapper">
-        <section id="main-content">
-             <p>balance </p>
-             <p>60 </p>
-         </section>
-      
-        <section id="sidebar">
-        <div>
-                <div class="container">
-                    <div class="timeline">
-                        <div class="timeline-event">
-                            <div class="card timeline-content">
-                                <div class="card-content">
-                                    <span class="card-title activator grey-text text-darken-4">Tile<i
-                                        class="material-icons right">more_vert</i></span>
+        return (
+            <div>
+                < section id="main-sections" >
+                    <div className="row" id ="main-content">
+                        <container>
+                            <h1>Total amount of outstanding requested time from others: </h1>
+                            <p>VARIABLE </p>
+                        </container>
+                    </div>
+                    <br></br>
+                    <div className="row" id ="main-content">
+                        <container>
+                            <h1>Total amount of outstanding payed time to others: </h1>
+                            <p>VARIABLE </p>
+                        </container>
+                    </div>
+                </section >
+                <div className="container" >
+                    <div className="timeline">
+                        <div className="timeline-event">
+                            <div className="card timeline-content">
+                                <div className="card-content">
+                                    <span className="card-title activator grey-text text-darken-4">Tile<i
+                                        className="material-icons right">more_vert</i></span>
                                 </div>
-                                <div class="card-reveal">
-                                    <span class="card-title grey-text text-darken-4">Card Title<i
-                                        class="material-icons right">close</i></span>
+                                <div className="card-reveal">
+                                    <span className="card-title grey-text text-darken-4">Card Title<i
+                                        className="material-icons right">close</i></span>
                                     <p>Here is some more information about this product that is only revealed once clicked on.</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="timeline-event">
-                            <div class="card timeline-content">
-                                <div class="card-content">
-                                    <span class="card-title activator grey-text text-darken-4">Tile<i
-                                        class="material-icons right">more_vert</i></span>
+                        <div className="timeline-event">
+                            <div className="card timeline-content">
+                                <div className="card-content">
+                                    <span className="card-title activator grey-text text-darken-4">Tile<i
+                                        className="material-icons right">more_vert</i></span>
                                 </div>
-                                <div class="card-reveal">
-                                    <span class="card-title grey-text text-darken-4">Card Title<i
-                                        class="material-icons right">close</i></span>
+                                <div className="card-reveal">
+                                    <span className="card-title grey-text text-darken-4">Card Title<i
+                                        className="material-icons right">close</i></span>
                                     <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="timeline-event">
-                            <div class="card timeline-content">
-                                <div class="card-content">
-                                    <span class="card-title activator grey-text text-darken-4">Tile<i
-                                        class="material-icons right">more_vert</i></span>
-                                </div>
-                                <div class="card-reveal">
-                                    <span class="card-title grey-text text-darken-4">Card Title<i
-                                        class="material-icons right">close</i></span>
-                                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="timeline-event">
-                            <div class="card timeline-content">
-                                <div class="card-content">
-                                    <span class="card-title activator grey-text text-darken-4">Tile<i
-                                        class="material-icons right">more_vert</i></span>
-                                </div>
-                                <div class="card-reveal">
-                                    <span class="card-title grey-text text-darken-4">Card Title<i
-                                        class="material-icons right">close</i></span>
-                                    <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-        </div>
-        </section>
-
-        </div>
-        //         <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-
-        //         <!--Compiled and minified JavaScript-- >
-        //             <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
-        //             <script>
-        //                 M.AutoInit();
-        // </script>
-    )
-}
+            </div>
+        )
+    }
 }
 
-export default Profile 
+export default Profile
+
+
