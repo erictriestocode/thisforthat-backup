288679e5668ff7622babddda06812fa1689c015e
violettebec <bechoerner@gmail.com>
2019-05-12 14:46:27 -0500

    sidenav working

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,16 +1,25 @@
+//set imports
 import React, { Component } from "react";
-// import { Link } from 'react-router-dom';
 import "./style.css";
-// import "./nav.js";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "materialize-css/dist/css/materialize.min.css";
 
 class Navbar extends Component {
+  componentDidMount() {
+    var elem = document.querySelector(".sidenav");
+    var instance = M.Sidenav.init(elem, {
+        edge: "left",
+        inDuration: 250
+    });
+}
+  
   render() {
     // const style = {
     //   color: 'white'
     // };
     return (
-
       <div>
+        <div>
         < nav >
           <div className="nav-wrapper">
             <a href="/home" className="brand-logo">ThisforThat</a>
@@ -30,14 +39,24 @@ class Navbar extends Component {
             </ul>
           </div>
         </nav >
+
         <ul className="sidenav" id="mobile-demo">
           <li><a href="/home">ThisforThat</a></li>
           <li><a href="/transaction">Transactions</a></li>
           <li><a href="/profile"><i className="fas fa-user-circle fa-lg"></i></a></li>
         </ul>
       </div>
-    );
-  }
+        <ul id="slide-out" className="sidenav"><li />
+          <li><a href="#!"><i className="material-icons">cloud</i>First LinkWith Icon</a></li>
+          <li><a href="#!">Second Link</a></li>
+          <li><div className="divider" /></li>
+          <li><a className="subheader">Subheader</a></li>
+          <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+        </ul>
+        <a href="#" data-target="slide-out" className="sidenav-trigger"></a>
+      </div>
+        );
+    }
 }
 
 export default Navbar
