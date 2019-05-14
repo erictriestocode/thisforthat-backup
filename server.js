773491a3f20170ef8874b197cc8e05dab351114f
erictriestocode <eric.northrop@gmail.com>
2019-05-14 17:12:17 -0500

    comment out additional code

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.use(express.static(__dirname + "/public/"));
 var syncOptions = { force: false };
 
 // HEROKU
-if (process.env.NODE_ENV === ' production'){
-  app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === ' production'){
+//   app.use(express.static('client/build'));
+// }
 // Routes
 require("./routes/apiRoutes")(app);
 app.get("*", (req, res) => {
