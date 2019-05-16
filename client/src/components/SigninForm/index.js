741b939fb21aa5fc7dea6474746de166d8faa8a3
Santiago tulande <santiagotulande@gmail.com>
2019-05-16 16:05:36 -0500

    add balances sent and rec

--- client/src/components/SigninForm/index.js
+++ client/src/components/SigninForm/index.js
@@ -6,76 +6,76 @@ import "./style.css";
 
 class SigninForm extends Component {
 
-    state = {
-        email: '',
-        password: ''
-    }
+  state = {
+    email: '',
+    password: ''
+  }
 
-    handleTextChange = evt => {
-        const { name, value } = evt.target
-        this.setState({ [name]: value })
-    }
+  handleTextChange = evt => {
+    const { name, value } = evt.target
+    this.setState({ [name]: value })
+  }
 
-    handleSubmit = evt => {
-        evt.preventDefault();
-        // functionToPushDataIntoDB(this.state)
-       // console.log(this.state);
-        const {email,password} = this.state;
-            axios
-            //HERE WE POST TO THE BACKEND ROUTES
-              .post('/api/login', {
-                email,
-                password
-              })
-              .then(response => {
-              //   console.log("response:", response);
-              //   this.setState({
-              //     messageFromServer: response.status,
-              //     showError: false,
-              //     loginError: false,
-              //     registerError: false,
-              //   });
-              //console.log(response);
-              if(response.status === 200){
-                this.props.history.push('/home');
-              } 
-              })
-              .catch(error => {
-                // console.log(error);
-                // console.log(error.response);
-              //   if (error.response.data === 'username or email already taken') {
-              //     this.setState({
-              //       showError: true,
-              //       loginError: true,
-              //       registerError: false,
-                  });
-                  
-                //}
-              //});
-          
-    }
+  handleSubmit = evt => {
+    evt.preventDefault();
+    // functionToPushDataIntoDB(this.state)
+    // console.log(this.state);
+    const { email, password } = this.state;
+    axios
+      //HERE WE POST TO THE BACKEND ROUTES
+      .post('/api/login', {
+        email,
+        password
+      })
+      .then(response => {
+        //   console.log("response:", response);
+        //   this.setState({
+        //     messageFromServer: response.status,
+        //     showError: false,
+        //     loginError: false,
+        //     registerError: false,
+        //   });
+        //console.log(response);
+        if (response.status === 200) {
+          this.props.history.push('/home');
+        }
+      })
+      .catch(error => {
+        // console.log(error);
+        // console.log(error.response);
+        //   if (error.response.data === 'username or email already taken') {
+        //     this.setState({
+        //       showError: true,
+        //       loginError: true,
+        //       registerError: false,
+      });
 
-render() {
+    //}
+    //});
 
-  return (
+  }
+
+  render() {
+
+    return (
       <div>
         <br></br>
         <br></br>
         <br></br>
         <div className="signin-container">
-        <div class="z-depth-1 grey lighten-4 row">
+          <div class="z-depth-1 grey lighten-4 row">
             <form onSubmit={this.handleSubmit}>
-              <label>Email</label><input type="text" name="email" onChange={this.handleTextChange}/>
-              <label>Password</label><input type="password" name="password" onChange={this.handleTextChange}/>
+              <label>Email</label><input type="text" name="email" onChange={this.handleTextChange} />
+              <label>Password</label><input type="password" name="password" onChange={this.handleTextChange} />
               <button type="submit" className="btn-large waves-effect waves- blue darken-4">Sign In</button>
             </form>
           </div>
         </div>
-    </div>
-  )
-}
+      </div>
+    )
+  }
 }
 
-export default  SigninForm
+export default SigninForm
 
 
--- client/src/components/Transaction/index.js
+++ client/src/components/Transaction/index.js
@@ -9,7 +9,7 @@ class Transaction extends Component {
 
     state = {
         Transaction_Desc: '',
-        Tokens_Transaction_amount: '', 
+        Tokens_Transaction_amount: '',
         User_Name_requesting_service: '',
         User_Name_Providing_service: '',
         //TotalTokensBalance: '',
@@ -23,7 +23,7 @@ class Transaction extends Component {
         const { name, value } = evt.target
         this.setState({ [name]: value })
     }
-    
+
     handleSubmit = evt => {
         evt.preventDefault();
         // functionToPushDataIntoDB(this.state)
@@ -31,102 +31,102 @@ class Transaction extends Component {
     }
 
     componentDidMount() {
-        this. UserRequesting();
+        this.UserRequesting();
         //this.UserIdRec();
-       //this.Newtransction();
-      }
-     
+        //this.Newtransction();
+    }
 
-   //grab user name requesting transaction from the API
-   UserRequesting = () => {
-    return axios.get('/api/findalltransUser')
-        .then((res) => {
-            this.setState({ User_Name_requesting_service: res.data[0].User_Name_requesting_service, UserId:res.data[0].UserId});
-            console.log("username sending : ", res.data[0].User_Name_requesting_service);
-            console.log("username sending : ", res.data[0].UserId);
-            
-        });
-};
-        transactionReq = e => {
-            console.log("here");
-            e.preventDefault();
-            const { Transaction_Desc, Tokens_Transaction_amount, User_Name_requesting_service, User_Name_Providing_service,UserId} = this.state;
-            if (Transaction_Desc === '' || Tokens_Transaction_amount === '' ||  User_Name_Providing_service === '') {
-                this.setState({
-                    showError: true,
-                    loginError: false,
-                    registerError: true,
-                });
-            } else{
-                axios.post('http://localhost:5000/api/sendTransRequest', {
+
+    //grab user name requesting transaction from the API
+    UserRequesting = () => {
+        return axios.get('/api/findalltransUser')
+            .then((res) => {
+                this.setState({ User_Name_requesting_service: res.data.dbtransaction[0].User_Name_requesting_service, UserId: res.data.dbtransaction[0].UserId });
+                console.log("username sending : ", res.data.dbtransaction[0].User_Name_requesting_service);
+                console.log("username sending : ", res.data.dbtransaction[0].UserId);
+
+            });
+    };
+    transactionReq = e => {
+        console.log("here");
+        e.preventDefault();
+        const { Transaction_Desc, Tokens_Transaction_amount, User_Name_requesting_service, User_Name_Providing_service, UserId } = this.state;
+        if (Transaction_Desc === '' || Tokens_Transaction_amount === '' || User_Name_Providing_service === '') {
+            this.setState({
+                showError: true,
+                loginError: false,
+                registerError: true,
+            });
+        } else {
+            axios.post('http://localhost:5000/api/sendTransRequest', {
                 Transaction_Desc,
-                Tokens_Transaction_amount, 
+                Tokens_Transaction_amount,
                 User_Name_requesting_service,
                 User_Name_Providing_service,
                 //TotalTokensBalance,
                 UserId
             })
-            .then(response => {
-            
-            if(response.status === 201){
-              this.props.history.push('/home');
-            } 
-            })
-            .catch(err => {
-              console.log(err);
-                });
-            }
-        };
+                .then(response => {
 
-    render(){
+                    if (response.status === 201) {
+                        this.props.history.push('/home');
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+
+    render() {
         return (
             <div>
-             <Navbar/>
+                <Navbar />
 
-            <div className="form-container">
-            <form id="TransactionForm" onSubmit={this.transactionReq}>
-                <div className="row">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        <p className="cool-font">Transaction</p>
-                        <input 
-                        className="form-control" 
-                        type="text" 
-                        name="Transaction_Desc" 
-                        placeholder="Write a description for your trasnaction"
-                        onChange={this.handleTextChange}
-                        />
-                        <br />
-                        <p className="cool-font">Tokens (Each Token equals 15 Minutes)</p>
-                        <input 
-                        className="form-control" 
-                        rows="3" 
-                        name="Tokens_Transaction_amount" 
-                        placeholder="Transaction amount"
-                        onChange={this.handleTextChange}
-                        />
-                        <br />
-                        {/* <p className="cool-font">Request time from...</p>
+                <div className="form-container">
+                    <form id="TransactionForm" onSubmit={this.transactionReq}>
+                        <div className="row">
+                            <div className="col-sm-8 col-sm-offset-2">
+                                <p className="cool-font">Transaction</p>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    name="Transaction_Desc"
+                                    placeholder="Write a description for your trasnaction"
+                                    onChange={this.handleTextChange}
+                                />
+                                <br />
+                                <p className="cool-font">Tokens (Each Token equals 15 Minutes)</p>
+                                <input
+                                    className="form-control"
+                                    rows="3"
+                                    name="Tokens_Transaction_amount"
+                                    placeholder="Transaction amount"
+                                    onChange={this.handleTextChange}
+                                />
+                                <br />
+                                {/* <p className="cool-font">Request time from...</p>
                         <input className="form-control" rows="3" id="User_Name_requesting_service" placeholder="Username of the user you are requesting from"></input> */}
 
-                        <br />
-                        <p className="cool-font">Send time to...</p>
-                        <input 
-                        className="form-control"
-                         rows="3" 
-                         Name="User_Name_Providing_service" 
-                         placeholder="Username of the user you want to send time to"
-                         onChange={this.handleTextChange}
-                         />
-                        <br />
-                        {/* <p className="cool-font">AailableTokensBalance</p>
+                                <br />
+                                <p className="cool-font">Send time to...</p>
+                                <input
+                                    className="form-control"
+                                    rows="3"
+                                    Name="User_Name_Providing_service"
+                                    placeholder="Username of the user you want to send time to"
+                                    onChange={this.handleTextChange}
+                                />
+                                <br />
+                                {/* <p className="cool-font">AailableTokensBalance</p>
                         <input className="form-control" rows="3" id="TotalTokensBalance" placeholder="Total Tokens Balance"></input> */}
 
-                        <button id="transreques-submit" className="btn btn-lg pull-right" href="profile.html">Submit!</button>
-                    </div>
-                    </div>
-                </form> 
-             </div>
-        </div>
+                                <button id="transreques-submit" className="btn btn-lg pull-right" href="profile.html">Submit!</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         );
     };
 }
