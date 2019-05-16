5674bb5a70c279738540a87153df5b04e9c0bf55
erictriestocode <eric.northrop@gmail.com>
2019-05-16 17:59:22 -0500

    add build

---
+++ client/build/precache-manifest.4511e4c92f7109d77866a567a2783c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2952281ce68cc716e5b0e6747168e22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5204c1d426e956de04ed",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "f3598b27525211411076",
+    "url": "/static/css/main.3a695254.chunk.css"
+  },
+  {
+    "revision": "5204c1d426e956de04ed",
+    "url": "/static/js/2.d21508d3.chunk.js"
+  },
+  {
+    "revision": "f3598b27525211411076",
+    "url": "/static/js/main.f0916d9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
