17368fe5f8429bdc81fbb2b616a6097bc5e6e373
erictriestocode <eric.northrop@gmail.com>
2019-05-16 16:33:20 -0500

    added comments

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ var PORT = process.env.PORT || 5000;
 app.use(session({ secret: "wewillsaveallofyourtime!", resave: true, saveUninitialized: true })); //ENV variable in heroku
 app.use(passport.initialize());
 app.use(passport.session());
+// ***** CORS *****
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
