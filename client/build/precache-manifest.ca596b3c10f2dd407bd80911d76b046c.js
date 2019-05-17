ca1f418c5885187070eea89218d6d653a82bb5e0
erictriestocode <eric.northrop@gmail.com>
2019-05-16 19:27:48 -0500

    Adding build file to github

---
+++ client/build/precache-manifest.ca596b3c10f2dd407bd80911d76b046c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3b9452671e55e23bac033127047459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5204c1d426e956de04ed",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "10db39a250f10bb04d2e",
+    "url": "/static/css/main.3a695254.chunk.css"
+  },
+  {
+    "revision": "5204c1d426e956de04ed",
+    "url": "/static/js/2.d21508d3.chunk.js"
+  },
+  {
+    "revision": "10db39a250f10bb04d2e",
+    "url": "/static/js/main.0e6d9c50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
